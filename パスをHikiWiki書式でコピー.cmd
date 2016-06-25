b6c79fe361f48aa9ecdbcfb471b418a33a72b702
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-06-26 00:58:50 +0900

    [Add] HikiWiki書式でコピー機能

---
+++ パスをHikiWiki書式でコピー.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+rem move to batch file's directory
+cd /d %~dp0
+
+rem %~f1: 1st arguments file's full path
+rem clip: copy input text (CScript's echo)
+CScript /NoLogo js\hikiwiki.js "%~f1" | clip
