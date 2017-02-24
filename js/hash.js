991c2f0203eccce666a5bec1c07edba010f37509
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-02-24 22:34:41 +0900

    [Clean] インデント変更: Space -> Tab

--- js/hash.js
+++ js/hash.js
@@ -2,10 +2,10 @@ var stdin = WScript.StdIn;
 var stdout = WScript.StdOut;
 
 while (!stdin.AtEndOfStream) {
-     var str = stdin.ReadLine();
-     if (/^[0-9a-f ]+$/.test(str)) {
-     	// ハッシュだけの行：スペースを消す
-     	str = "\n" + str.replace(/ /g, "") + "\n";
-     }
-     stdout.WriteLine(str);
+	var str = stdin.ReadLine();
+	if (/^[0-9a-f ]+$/.test(str)) {
+		// ハッシュだけの行：スペースを消す
+		str = "\n" + str.replace(/ /g, "") + "\n";
+	}
+	stdout.WriteLine(str);
 }
